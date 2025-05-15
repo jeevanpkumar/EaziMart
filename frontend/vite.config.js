b38cecd8c357551ce78784de2211b8176668886b
Jeevan <jeevanpkumarnr2016@gmail.com>
2025-05-15 17:52:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 800, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor'
-            if (id.includes('lodash')) return 'lodash'
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
 })
