16c1d4222a9b92f3631c5fdb200907ff4e1579af
Jeevan <jeevanpkumarnr2016@gmail.com>
2025-05-17 14:10:12 +0530

    updated Multiple

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,31 +1,21 @@
 const express = require("express");
 const { stripe } = require("../lib/stripe.js");
 const { protect } = require("../middleware/authMiddleware.js");
-const Checkout = require("../models/Checkout");
 
 const router = express.Router();
 
 router.post("/create-stripe-session", protect, async (req, res) => {
   try {
-    const { products, checkoutId } = req.body;
+    const { products } = req.body;
 
     if (!Array.isArray(products) || products.length === 0) {
       return res.status(400).json({ error: "Invalid or empty products array" });
     }
 
-    if (!checkoutId) {
-      return res.status(400).json({ error: "Missing checkoutId" });
-    }
-
-    const checkout = await Checkout.findById(checkoutId);
-    if (!checkout) {
-      return res.status(404).json({ error: "Checkout not found" });
-    }
-
     let totalAmount = 0;
 
     const lineItems = products.map((product) => {
-      const amount = Math.round(product.price * 100); // Stripe uses cents
+      const amount = Math.round(product.price * 100); // Stripe requires amounts in cents
       totalAmount += amount * product.quantity;
       return {
         price_data: {
@@ -39,16 +29,15 @@ router.post("/create-stripe-session", protect, async (req, res) => {
         quantity: product.quantity || 1,
       };
     });
-
+console.log("frontend Url", process.env.FRONTEND_URL)
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.FRONTEND_URL}/order-confirmation?session_id={CHECKOUT_SESSION_ID}&checkout_id=${checkoutId}`,
+      success_url: `${process.env.FRONTEND_URL}/order-confirmation?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.FRONTEND_URL}/purchase-cancel`,
       metadata: {
         userId: req.user._id.toString(),
-        checkoutId: checkoutId,
         products: JSON.stringify(
           products.map((p) => ({
             id: p._id,
@@ -62,10 +51,11 @@ router.post("/create-stripe-session", protect, async (req, res) => {
     res.status(200).json({ id: session.id, url: session.url, totalAmount: totalAmount / 100 });
 
   } catch (error) {
-    console.error("Error processing Stripe session:", error);
-    res.status(500).json({ message: "Error processing Stripe session", error: error.message });
+    console.error("Error processing checkout:", error);
+    res.status(500).json({ message: "Error processing checkout", error: error.message });
   }
 });
 
 module.exports = router;
+
 
